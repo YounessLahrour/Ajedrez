e72f2918c8dc1a0a98d18349e656e51c9fbdbb8f
Youness <Youness@192.168.12.199>
2019-04-09 21:49:10 +0200

    Ejer 12. creo los setters y los getters de la clase caballo

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -13,4 +13,22 @@ public class Caballo
 {
 private Color color;
 private Posicion posicion;
+
+    public Color getColor() {
+        return color;
+    }
+
+    private void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    private void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+
 }
