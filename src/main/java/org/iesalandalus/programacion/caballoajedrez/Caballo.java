bb783686c2a1cd5d0d4b2745a7e3a2699263281f
Youness <Youness@192.168.12.199>
2019-04-10 18:08:10 +0200

    Ejer 15 y 16. creo el constructor para cada columna y color, y creo el constructor por copia

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -41,6 +41,33 @@ private Posicion posicion;
         if(color==Color.NEGRO)
             this.color=color;
             this.posicion= new Posicion(8,'b');
+    }     
+    public Caballo(Color color, char columna){
         
+        if(columna=='b' || columna=='g'){
+            
+            if(color==Color.BLANCO){
+            this.color=color;
+            this.posicion= new Posicion(1,columna);}
+            else{
+                this.color=color;
+                this.posicion= new Posicion(8,columna);
+            }
+        } else throw new IllegalArgumentException("ERROR: Has pasado un parametro nulo");
+                    }
+    public Caballo(Caballo Caballo1){
+        if(Caballo1==null)
+        throw new IllegalArgumentException("ERROR: No es posible copiar un caballo nulo.");
+        else
+        this.color=Caballo1.color;
+        this.posicion=Caballo1.posicion;
+
     }
-}
+            }
+        
+       
+   
+    
+        
+    
+
