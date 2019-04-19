134145697b67a86fcba97137263e3c2163bb3ed5
Youness <Youness@192.168.12.199>
2019-04-20 01:29:04 +0200

    Ejer 18. creo el metodo toString

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Caballo.java
@@ -12,10 +12,10 @@ import javax.naming.OperationNotSupportedException;
  *
  * @author Youness
  */
-public class Caballo 
-{
-private Color color;
-private Posicion posicion;
+public class Caballo {
+
+    private Color color;
+    private Posicion posicion;
 
     public Color getColor() {
         return color;
@@ -33,117 +33,121 @@ private Posicion posicion;
         this.posicion = posicion;
     }
 
-    public Caballo(){
-        
-        this.color=Color.NEGRO;
-        this.posicion= new Posicion(8,'b');
+    public Caballo() {
+
+        this.color = Color.NEGRO;
+        this.posicion = new Posicion(8, 'b');
     }
-    public Caballo(Color color)
-    {
-        if(color==null) throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
-            this.color=color;
-            
-        if(color==Color.BLANCO)
-            this.color=color;
-            this.posicion= new Posicion(1,'b');
-        if(color==Color.NEGRO)
-            this.color=color;
-            this.posicion= new Posicion(8,'b');
-    }     
-    public Caballo(Color color, char columna)
-    {
-       if (color==null) throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo."); 
-        
-        if(columna=='b' || columna=='g'){
-            
-            if(color==Color.BLANCO){
-            this.color=color;
-            this.posicion= new Posicion(1,columna);}
-            else{
-                this.color=color;
-                this.posicion= new Posicion(8,columna);
+
+    public Caballo(Color color) {
+        if (color == null) {
+            throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+        }
+        this.color = color;
+
+        if (color == Color.BLANCO) {
+            this.color = color;
+            this.posicion = new Posicion(1, 'b');
+        } else {
+            this.color = color;
+            this.posicion = new Posicion(8, 'b');
+        }
+    }
+
+    public Caballo(Color color, char columna) {
+        if (color == null) {
+            throw new IllegalArgumentException("ERROR: No se puede asignar un color nulo.");
+        }
+
+        if (columna == 'b' || columna == 'g') {
+
+            if (color == Color.BLANCO) {
+                this.color = color;
+                this.posicion = new Posicion(1, columna);
+            } else {
+                this.color = color;
+                this.posicion = new Posicion(8, columna);
             }
-        } else throw new IllegalArgumentException("ERROR: Has pasado un parametro nulo");
-                    }
-    
-    public void mover(Direccion nuevaDireccion) throws OperationNotSupportedException
-    {
-      int fila=posicion.getFila();
-      int nuevaFila;
-      char columna=posicion.getColumna();
-      char nuevaColumna;
-      
-      try{
-      
-          switch(nuevaDireccion)
-          {
-              case ARRIBA_IZQUIERDA:
-                  nuevaFila=fila+2;
-                  nuevaColumna=(char)(columna-1);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-                  
-              break;
-              
-              case ARRIBA_DERECHA:
-                  nuevaFila=fila+2;
-                  nuevaColumna=(char)(columna+1);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;
-              
-              case DERECHA_ABAJO:
-                  nuevaFila=fila-1;
-                  nuevaColumna=(char)(columna+2);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;
-                  
-              case DERECHA_ARRIBA:
-                  nuevaFila=fila+1;
-                  nuevaColumna=(char)(columna+2);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;    
-                
-              case IZQUIERDA_ABAJO:
-                  nuevaFila=fila-2;
-                  nuevaColumna=(char)(columna-1);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;  
-              
-              case IZQUIERDA_ARRIBA:
-                  nuevaFila=fila-2;
-                  nuevaColumna=(char)(columna+1);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;
-              
-              case ABAJO_DERECHA:
-                  nuevaFila=fila-2;
-                  nuevaColumna=(char)(columna+1);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;
-              
-              case ABAJO_IZQUIERDA:
-                  nuevaFila=fila-2;
-                  nuevaColumna=(char)(columna-1);
-                  posicion.setFila(nuevaFila);
-                  posicion.setColumna(nuevaColumna);
-              break;
-              default:
-                  throw new OperationNotSupportedException("Movimiento erróneo");
-          }
-          }catch (IllegalArgumentException e) 
-          {
+        } else {
+            throw new IllegalArgumentException("ERROR: Columna inicial no válida.");
+        }
+    }
+
+    public void mover(Direccion nuevaDireccion) throws OperationNotSupportedException {
+        int fila = posicion.getFila();
+        int nuevaFila;
+        char columna = posicion.getColumna();
+        char nuevaColumna;
+
+        try {
+
+            switch (nuevaDireccion) {
+                case ARRIBA_IZQUIERDA:
+                    nuevaFila = fila + 2;
+                    nuevaColumna = (char) (columna - 1);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+
+                    break;
+
+                case ARRIBA_DERECHA:
+                    nuevaFila = fila + 2;
+                    nuevaColumna = (char) (columna + 1);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+
+                case DERECHA_ABAJO:
+                    nuevaFila = fila - 1;
+                    nuevaColumna = (char) (columna + 2);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+
+                case DERECHA_ARRIBA:
+                    nuevaFila = fila + 1;
+                    nuevaColumna = (char) (columna + 2);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+
+                case IZQUIERDA_ABAJO:
+                    nuevaFila = fila + 2;
+                    nuevaColumna = (char) (columna - 1);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+
+                case IZQUIERDA_ARRIBA:
+                    nuevaFila = fila - 2;
+                    nuevaColumna = (char) (columna + 1);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+
+                case ABAJO_DERECHA:
+                    nuevaFila = fila - 2;
+                    nuevaColumna = (char) (columna + 1);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+
+                case ABAJO_IZQUIERDA:
+                    nuevaFila = fila - 2;
+                    nuevaColumna = (char) (columna - 1);
+                    posicion.setFila(nuevaFila);
+                    posicion.setColumna(nuevaColumna);
+                    break;
+                default:
+                    throw new OperationNotSupportedException("Movimiento erróneo");
+            }
+        } catch (IllegalArgumentException e) {
             posicion.setColumna(columna);
             posicion.setFila(fila);
             throw new OperationNotSupportedException("ERROR: Movimiento no válido.");
-           }
-              
-          }
+        }
+
+    }
 
     @Override
     public int hashCode() {
@@ -173,14 +177,10 @@ private Posicion posicion;
         }
         return true;
     }
-        
-    }
-    
-            
-        
-       
-   
-    
-        
-    
 
+    @Override
+    public String toString() {
+        return "Caballo [posición=" + posicion + ", color=" + color + "]";
+    }
+
+}
